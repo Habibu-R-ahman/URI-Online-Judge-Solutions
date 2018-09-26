01b42eda965716b7e96d087140a1cd207c1ca098
b1ackc0d3 <35610644+HimelAhmed@users.noreply.github.com>
2018-09-26 14:39:08 +0600

    Add files via upload

---
+++ All Codes/URI_1002 .c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#define PI 3.14159
+int main()
+{
+    double R;
+    scanf("%lf",&R);
+    printf("A=%.4f\n",PI*R*R);
+    return 0;
+}
---
+++ All Codes/URI_1003.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int A, B;
+    scanf("%d%d", &A, &B);
+    printf("SOMA = %d\n", A+B);
+    return 0;
+}

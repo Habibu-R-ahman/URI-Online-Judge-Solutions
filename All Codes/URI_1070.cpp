36b34f795036bdb1e4dfdb34eb3248275018c90b
b1ackc0d3 <35610644+HimelAhmed@users.noreply.github.com>
2019-07-12 00:53:46 +0600

    Update URI_1070.cpp

--- All Codes/URI_1070.cpp
+++ All Codes/URI_1070.cpp
@@ -2,23 +2,16 @@
 
 using namespace std;
 
-int main() {
-	
-    int X,i;
-    cin >> X;
-    
-    for (i=0; i<11; i++) {
-        a:
-        if (X%2 != 0) {
-            cout << X << endl;
-            i++;
-            X++;
-        }
-        else {
-            X++;
-            goto a;
-        }
-    }
-    return 0;
-}
+int main(){
 
+	int a,i,j;
+	cin >> a;
+
+	for(i=a, j=0; j < 6; i++){
+		if(i % 2 != 0){
+			cout << i << "\n";
+			j++;
+		}
+	}
+	return 0;
+}

56416298df587999253b4e2e6e43584260a28f7b
Habib <anuhimel@gmail.com>
2020-07-26 21:24:02 +0600

    Update

---
+++ All Codes/URI_2748.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "---------------------------------------" << "\n";
+    cout << "|        Roberto                      |" << "\n";
+    cout << "|                                     |" << "\n";
+    cout << "|        5786                         |" << "\n";
+    cout << "|                                     |" << "\n";
+    cout << "|        UNIFEI                       |" << "\n";
+    cout << "---------------------------------------" << "\n";
+    
+    return 0;
+}
---
+++ All Codes/URI_2749.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("---------------------------------------\n");
+    printf("|x = 35                               |\n");
+    printf("|                                     |\n");
+    printf("|               x = 35                |\n");
+    printf("|                                     |\n");
+    printf("|                               x = 35|\n");
+    printf("---------------------------------------\n");
+    
+    return 0;
+}

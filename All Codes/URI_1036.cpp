93173b00deecddf0ebb26a17d9c349a07bcf6fb5
Habibur Rahman <anuhimel@gmail.com>
2021-01-05 10:34:20 +0600

    Update

--- All Codes/URI_1036.cpp
+++ All Codes/URI_1036.cpp
@@ -13,8 +13,8 @@ int main() {
     
     if(A != 0 && root > 0)
     {
-        R1 = (- B + sqrt( B * B - 4 * A * C )) / (2 * A);
-        R2 = (- B - sqrt( B * B - 4 * A * C )) / (2 * A);
+        R1 = (- B + sqrt(root)) / (2 * A);
+        R2 = (- B - sqrt(root)) / (2 * A);
         
         cout << "R1 = " << fixed << setprecision(5) << R1 << endl;
         cout << "R2 = " << fixed << setprecision(5) << R2 << endl;

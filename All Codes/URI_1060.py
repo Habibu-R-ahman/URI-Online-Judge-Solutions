7393a6110c4736a57e5b49ede6e7c6f9faa37bb2
Habibur Rahman <anuhimel@gmail.com>
2021-01-08 23:14:51 +0600

    Python Solution Added

--- All Codes/URI_1060.py
+++ All Codes/URI_1060.py
@@ -1,5 +1,4 @@
 ans = 0
 for x in range(6):
-    a = float(input())
-    ans += a > 0
+    ans += float(input()) > 0
 print(f"{ans} valores positivos")
---
+++ All Codes/URI_1065.py
@@ -0,0 +1,4 @@
+ans = 0
+for x in range(5):
+    ans += float(input()) % 2 == 0
+print(f"{ans} valores pares")

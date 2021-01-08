6b47b14f0be44f6576f304908f99d727f707feaf
Habibur Rahman <anuhimel@gmail.com>
2021-01-08 23:25:12 +0600

    Not yet solved

---
+++ All Codes/URI_1066.py
@@ -0,0 +1,10 @@
+# even = odd = pos = neg = 0
+# for x in range(5):
+#     even += int(input()) % 2 == 0
+#     odd += int(input()) % 2 != 0
+#     pos += int(input()) > 0
+#     neg += int(input()) < 0
+# print(f"{even} valor(es) par(es)")
+# print(f"{odd} valor(es) impar(es)")
+# print(f"{pos} valor(es) positivo(s)")
+# print(f"{neg} valor(es) negativo(s)")

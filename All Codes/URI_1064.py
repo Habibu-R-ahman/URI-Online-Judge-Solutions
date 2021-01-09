3b54aa2ffab011cd927a144ee908239e406d82e8
Habibur Rahman <anuhimel@gmail.com>
2021-01-09 12:03:13 +0600

    Python Solution Added

--- All Codes/URI_1064.py
+++ All Codes/URI_1064.py
@@ -1,1 +1,6 @@
-# not yet solved
+lst = []
+for x in range(6):
+    ans = float(input())
+    lst.append(ans) if ans > 0.0 else None
+print(f"{len(lst)} valores positivos")
+print(f"{sum(lst)/len(lst):.1f}")

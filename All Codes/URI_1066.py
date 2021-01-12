62aa23f4bfcf9ea2a1054451c6380ed827816b3c
Habibur Rahman <anuhimel@gmail.com>
2021-01-12 23:26:13 +0600

    Not yet solved!

--- All Codes/URI_1066.py
+++ All Codes/URI_1066.py
@@ -1,7 +1,16 @@
-# ans = []
+# # ans = []
+# pos = []
+# neg = []
+# odd = []
+# evn = []
 # for x in range(5):
-#     ans.append(int(input()))
-#     for a, b in [(), (), ()]
+#     ans = int(input())
+#     evn = filter(lambda x: x % 2 == 0, ans)
+#     evn = filter(lambda x: x % 2 == 0, ans)
+#     evn = filter(lambda x: x % 2 == 0, ans)
+#     evn = filter(lambda x: x % 2 == 0, ans)
+#     # evn.append(ans) if ans % 2 == 0 else odd.append(ans)
+#     # pos.append(ans) if ans > 0 else neg.append(ans)
 # print(f"{len(evn)} valor(es) par(es)")
 # print(f"{len(odd)} valor(es) impar(es)")
 # print(f"{len(pos)} valor(es) positivo(s)")
